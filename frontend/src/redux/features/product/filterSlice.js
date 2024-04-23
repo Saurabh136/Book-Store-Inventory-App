d947deceafe63950650650c636c3094141e79d38
Saurabh Mhamunkar <smhamunkar2610@hotmail.com>
2024-04-23 21:40:02 +0530

    first version

--- frontend/src/redux/features/product/filterSlice.js
+++ frontend/src/redux/features/product/filterSlice.js
@@ -10,18 +10,19 @@ const filterSlice = createSlice({
   reducers: {
     FILTER_PRODUCTS(state, action) {
       const { products, search } = action.payload;
-      
+    
       const tempProducts = products.filter((product) => {
-        const titleLower = product.title ? product.title.toLowerCase() : "";
-        const genreLower = product.genre ? product.genre.toLowerCase() : "";
+        const titleLower = product.title && typeof product.title === "string" ? product.title.toLowerCase() : "";
+        const genreLower = product.genre && typeof product.genre === "string" ? product.genre.toLowerCase() : "";
         return (
           titleLower.includes(search.toLowerCase()) ||
           genreLower.includes(search.toLowerCase())
         );
       });
-
+    
       state.filteredProducts = tempProducts;
     },
+    
   },
 });
 
