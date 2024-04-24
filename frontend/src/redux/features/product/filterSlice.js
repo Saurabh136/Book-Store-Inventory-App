cc08c9306d0b755cfcb612bd905474c58dcaa767
Saurabh Mhamunkar <smhamunkar2610@hotmail.com>
2024-04-24 12:15:59 +0530

    first version

--- frontend/src/redux/features/product/filterSlice.js
+++ frontend/src/redux/features/product/filterSlice.js
@@ -12,11 +12,32 @@ const filterSlice = createSlice({
       const { products, search } = action.payload;
     
       const tempProducts = products.filter((product) => {
-        const titleLower = product.title && typeof product.title === "string" ? product.title.toLowerCase() : "";
-        const genreLower = product.genre && typeof product.genre === "string" ? product.genre.toLowerCase() : "";
+        const titleLower = (product.title && typeof product.title === "string") ? product.title.toLowerCase() : "";
+        const genreLower = (product.genre && typeof product.genre === "string") ? product.genre.toLowerCase() : "";
+        const seriesLower = (product.series && typeof product.series === "string") ? product.series.toLowerCase() : "";
+        const serialNumberLower = (product.serialnumber && typeof product.serialnumber === "string") ? product.serialnumber.toLowerCase() : "";
+        const primaryAuthorLower = (product.primaryauthor && typeof product.primaryauthor === "string") ? product.primaryauthor.toLowerCase() : "";
+        const secondaryAuthorLower = (product.secondaryauthor && typeof product.secondaryauthor === "string") ? product.secondaryauthor.toLowerCase() : "";
+        const editorLower = (product.editor && typeof product.editor === "string") ? product.editor.toLowerCase() : "";
+        const publisherLower = (product.publisher && typeof product.publisher === "string") ? product.publisher.toLowerCase() : "";
+        const editionLower = (product.edition && typeof product.edition === "string") ? product.edition.toLowerCase() : "";
+        const conditionLower = (product.condition && typeof product.condition === "string") ? product.condition.toLowerCase() : "";
+        const descriptionLower = (product.description && typeof product.description === "string") ? product.description.toLowerCase() : "";
+        const imageLower = (product.image && typeof product.image === "string") ? product.image.toLowerCase() : "";
+
         return (
           titleLower.includes(search.toLowerCase()) ||
-          genreLower.includes(search.toLowerCase())
+          genreLower.includes(search.toLowerCase()) ||
+          seriesLower.includes(search.toLowerCase()) ||
+          serialNumberLower.includes(search.toLowerCase()) ||
+          primaryAuthorLower.includes(search.toLowerCase()) ||
+          secondaryAuthorLower.includes(search.toLowerCase()) ||
+          editorLower.includes(search.toLowerCase()) ||
+          publisherLower.includes(search.toLowerCase()) ||
+          editionLower.includes(search.toLowerCase()) ||
+          conditionLower.includes(search.toLowerCase()) ||
+          descriptionLower.includes(search.toLowerCase()) ||
+          imageLower.includes(search.toLowerCase())
         );
       });
     
