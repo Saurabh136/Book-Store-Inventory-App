dceb891108a43e98d78273f48f03d9f9b3e0e3fa
Saurabh Mhamunkar <smhamunkar2610@hotmail.com>
2024-04-24 20:57:52 +0530

    first version

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -10,7 +10,9 @@ const productSchema = mongoose.Schema(
     title: {
       type: String,
       required: [true, "Please add a title"],
+      default: "N/A" ,
       trim: true,
+      
     },
     sku: {
       type: String,
@@ -21,62 +23,79 @@ const productSchema = mongoose.Schema(
     genre: {
       type: String,
       required: [true, "Please add a genre"],
+      default: "N/A" ,
       trim: true,
     },
     quantity: {
       type: Number,
       required: [true, "Please add a quantity"],
+      default: 0, 
       trim: true,
     },
     price: {
       type: Number,
       required: [true, "Please add a price"],
+      default: 0,
       trim: true,
     },
     series: {
       type: String,
       required: [true, "Please add Series name"],
-      trim: true,
+      default: "N/A" ,
+      trim: true,      
     },
     serialnumber: {
       type: Number,
       required: [true, "Please add Series Serial number"],
+      default: 0,
       trim: true,
     },
     primaryauthor: {
       type: String,
       required: [true, "Please add Primary Author"],
-      trim: true,
+      default: "N/A",
+      trim: true, 
     },
     secondaryauthor: {
       type: String,
       required: [true, "Please add a Secondary Author"],
+      default: "N/A" ,
       trim: true,
     },
     editor: {
       type: String,
       required: [true, "Please add a Editor"],
+      default: "N/A",
       trim: true,
+       
     },
     publisher: {
       type: String,
       required: [true, "Please add a Publisher"],
+      default: "N/A",
       trim: true,
+      
     },
     edition: {
       type: String,
       required: [true, "Please add a Edition"],
+      default: "N/A",
       trim: true,
+       
     },
     description: {
       type: String,
       required: [true, "Please add a description"],
+      default: "N/A",
       trim: true,
+       
     },
     condition: {
       type: String,
       required: [true, "Please add book condition"],
+      default: "N/A",
       trim: true,
+       
     },
     image: {
       type: Object,
